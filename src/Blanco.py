2b1516899962280c9c36415ee98e9e4bda828fd7
josephhatfield211 <jhjoeman224@gmail.com>
2018-11-05 17:36:17 -0500

    done maybe

---
+++ src/Blanco.py
@@ -0,0 +1,82 @@
+"""
+  Capstone Project.  Code written by PUT_YOUR_NAME_HERE.
+  Fall term, 2018-2019.
+"""
+
+import rosebotics as rb
+import rosebotics_new as rb2
+import ev3dev.ev3 as ev3
+import time
+
+
+def proximity_test():
+
+    print('Running!')
+    infrared = rb2.InfraredAsProximitySensor(ir_sensor_port=ev3.INPUT_4)
+
+    while True:
+
+        if infrared.get_distance_to_nearest_object() < 20:
+            ev3.Sound.beep().wait()
+            ev3.Sound.speak("Object is close!").wait()
+            break
+
+
+def follow_black_circle():
+    """ Runs YOUR specific part of the project """
+    robot = rb.Snatch3rRobot()
+    drive = robot.drive_system
+    color = robot.color_sensor
+    turn_right = True
+
+    while True:
+        # Moving forward until light is reflected
+        drive.start_moving()
+        color.wait_until_intensity_is_greater_than(10)
+
+        # Try turning clockwise (to the right)
+        counter = 0
+        while True:
+
+            if turn_right is True:
+                drive.start_moving(100, 0)
+            else:
+                drive.start_moving(0, 100)
+
+            # Runs if robot is back on black within 0.25 seconds (implying that robot is turning in right direction)
+            if color.get_reflected_intensity() < 10:
+                break
+
+            # Runs if robot has not encountered black after 0.25 seconds in direction it's turning
+            if counter > 0.5 and color.get_reflected_intensity() > 10:
+                print('No black detected, turn around!')
+
+                # Revert to original position
+                if turn_right is True:
+                    drive.move_for_seconds(1, -100, 0)
+                else:
+                    drive.move_for_seconds(1, 0, -100)
+
+                # Changes direction to turn in the future
+                if turn_right is True:
+                    turn_right = False
+                else:
+                    turn_right = True
+                print('Turn direction changed! Now turn_right is :', turn_right)
+
+                # Turn for certain amount of time
+                while True:
+                    if turn_right is True:
+                        drive.start_moving(100, 0)
+                    else:
+                        drive.start_moving(0, 100)
+
+                    if color.get_reflected_intensity() < 10:
+                        break
+
+            counter = counter + 0.05
+            print(counter, color.get_reflected_intensity())
+            time.sleep(0.05)
+
+
+proximity_test()
---
+++ src/YuanningZuo.py
@@ -0,0 +1,61 @@
+"""
+  Capstone Project.  Code written by Yuanning Zuo.
+  Fall term, 2018-2019.
+"""
+
+import rosebotics_new as rb
+import ev3dev.ev3 as ev3
+import time
+
+
+def main():
+    robot = rb.Snatch3rRobot()
+    # print()
+    # print("Testing touch sensor")
+    # print("CTRL-C to terminate the program!")
+    # time.sleep(1)
+    # count = 1
+    # while True:
+    #     print("{:4}.".format(count),"touch sensor value is: ",robot.touch_sensor.get_value())
+    #     time.sleep(0.5)
+    #     count = count +1
+
+# def findingcolour(color):
+#     robot = rb.Snatch3rRobot()
+#     print()
+#     print("Testing!")
+#     time.sleep(2)
+#     print("Testing!!")
+#     time.sleep(3)
+#     print("Testing!!!")
+#     time.sleep(3)
+#     print("CTRL-C to TERMINATE the program!!!!!!!!!")
+#     time.sleep(1)
+#     while True:
+#         robot.drive_system.go_straight_inches(1, 100)
+#         if robot.color_sensor.get_color() == (color.value):
+#             break
+#     robot.drive_system.stop_moving()
+
+def finding_object():
+    robot=rb.Snatch3rRobot()
+    print()
+    print("Testing")
+    time.sleep(2)
+    print("Testing!")
+    time.sleep(2)
+    print("Ctrl-C to TERMINATE!! the program!!!!!")
+    time.sleep(3)
+
+    while True:
+        if robot.camera.get_biggest_blob().get_area() >= 300:
+            ev3.Sound.beep().wait()
+            ev3.Sound.speak("It is huge").wait()
+            break
+    robot.drive_system.stop_moving()
+
+
+# findingcolour(rb.Color.BLUE)
+finding_object()
+
+
