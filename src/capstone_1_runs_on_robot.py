7af75f39e798a984bb9cba699e527d78d4efcc0b
josephhatfield211 <jhjoeman224@gmail.com>
2018-11-07 11:06:04 -0500

    done maybe

--- src/capstone_1_runs_on_robot.py
+++ src/capstone_1_runs_on_robot.py
@@ -7,17 +7,9 @@ Also: responds to Beacon button-presses by beeping, speaking.
 This module runs on the ROBOT.
 It uses MQTT to RECEIVE information from a program running on the LAPTOP.
 
-Authors:  David Mutchler, his colleagues, and PUT_YOUR_NAME_HERE.
+Authors:  David Mutchler, his colleagues, and Joey Hatfield.
 """
-# ------------------------------------------------------------------------------
-# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.  Then delete this TODO.
-# ------------------------------------------------------------------------------
 
-# ------------------------------------------------------------------------------
-# TODO: 2. With your instructor, review the "big picture" of laptop-robot
-# TODO:    communication, per the comment in mqtt_sender.py.
-# TODO:    Once you understand the "big picture", delete this TODO.
-# ------------------------------------------------------------------------------
 
 import rosebotics_new as rb
 import time
@@ -26,37 +18,32 @@ import ev3dev.ev3 as ev3
 
 
 def main():
-    # --------------------------------------------------------------------------
-    # TODO: 3. Construct a Snatch3rRobot.  Test.  When OK, delete this TODO.
-    # --------------------------------------------------------------------------
 
-    # --------------------------------------------------------------------------
-    # TODO: 4. Add code that constructs a   com.MqttClient   that will
-    # TODO:    be used to receive commands sent by the laptop.
-    # TODO:    Connect it to this robot.  Test.  When OK, delete this TODO.
-    # --------------------------------------------------------------------------
-
-    # --------------------------------------------------------------------------
-    # TODO: 5. Add a class for your "delegate" object that will handle messages
-    # TODO:    sent from the laptop.  Construct an instance of the class and
-    # TODO:    pass it to the MqttClient constructor above.  Augment the class
-    # TODO:    as needed for that, and also to handle the go_forward message.
-    # TODO:    Test by PRINTING, then with robot.  When OK, delete this TODO.
-    # --------------------------------------------------------------------------
-
-    # --------------------------------------------------------------------------
-    # TODO: 6. With your instructor, discuss why the following WHILE loop,
-    # TODO:    that appears to do nothing, is necessary.
-    # TODO:    When you understand this, delete this TODO.
-    # --------------------------------------------------------------------------
+    robot = rb.Snatch3rRobot()
+    remote = RemoteControlEtc(robot)
+    client = com.MqttClient(remote)
+    client.connect_to_pc()
     while True:
-        # ----------------------------------------------------------------------
-        # TODO: 7. Add code that makes the robot beep if the top-red button
-        # TODO:    on the Beacon is pressed.  Add code that makes the robot
-        # TODO:    speak "Hello. How are you?" if the top-blue button on the
-        # TODO:    Beacon is pressed.  Test.  When done, delete this TODO.
-        # ----------------------------------------------------------------------
+        if robot.beacon_button_sensor.is_top_red_button_pressed():
+            ev3.Sound.beep().wait()
+        if robot.beacon_button_sensor.is_top_blue_button_pressed():
+            ev3.Sound.speak('Hi').wait()
         time.sleep(0.01)  # For the delegate to do its work
 
+class RemoteControlEtc(object):
+    def __init__(self, robot):
+        """
+        Stores the robot.
+         :type robot: rb.Snatch3rRobot
+        """
+        self.robot = robot
+
+    def go_forward(self, speed_string):
+        #Makes robot go forward at the given speed
+        print('Telling the robot to start moving at', speed_string)
+        speed = int(speed_string)
+        self.robot.drive_system.start_moving(speed, speed)
+        time.sleep(5)
+        self.robot.drive_system.stop_moving()
 
 main()
